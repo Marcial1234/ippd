0599e80128d10dbdb80dabec5bb3ac1564273960
Alex <aeiffert@ufl.edu>
2018-03-16 00:04:28 -0400

    Gets the job done

--- devel/image_upload/server/index.js
+++ devel/image_upload/server/index.js
@@ -15,17 +15,54 @@ cloudinary.config({
 var upload_stream
 
 function uploadToServer(image, res) {
-  let date = Date.now();
-  console.log("Uploading " + image.name + " as: to_pano_" + date + ".png");
+  let id = Date.now();  // Time in milliseconds
+  let path = __dirname + "/public/upload/" + id + ".jpg";
+  let panoPath = __dirname + "/public/upload/" + id + "_pano.jpg";
+  console.log("Uploading " + image.name + " as " + path);
 
-  image.mv(__dirname + "/public/upload/to_pano_" + date + ".png", function(err) {
+  image.mv(path, function(err) {
     if(err)
       return res.status(500).send(err);
     res.write("Image(s) uploaded");
   });
+
+  return {
+    id: id,
+    path: path,
+    panoPath: panoPath,
+    name : image.name
+  };
 } 
 
-function processImages() {
+function processAndUploadImage(image) {
+  var stitch = exec(__dirname + '/public/gear360pano/gear360pano.sh -n ' + 
+                    '-r -o ' + __dirname + '/public/upload ' + image.path,
+                    (error, stdout, stderr) => {
+    console.log(`${stdout}`);
+    console.log(`${stderr}`);
+    if(error !== null)
+      console.log(`exec error: ${error}`);
+  });
+
+  stitch.on('close', () => {
+    console.log("Stitching complete for " + image.path + ". \nPushing to cloud.");
+    pushToCloud(image);
+  });
+  stitch.on('error', () => {
+    console.log("Error processing image " + image.path);
+  });
+}
+
+function pushToCloud(image) {
+  cloudinary.v2.uploader.upload(image.panoPath, 
+    {
+      resource_type: "image",
+      public_id: image.id,
+      overwrite: true
+    },
+    function(error, result) {
+      console.log(result);
+    });
 }
 
 /**
@@ -48,68 +85,17 @@ app.post('/upload', function(req, res) {
     for(var i = 0; i < req.files.image.length; ++i) {
       let image = formData[i];
       uploadToServer(image, res);
+      processAndUploadImage(image.path);
     }
   }
   else if(formData) {
     console.log("Files incoming: 1");
-    uploadToServer(formData, res);
+    let image = uploadToServer(formData, res);
+    processAndUploadImage(image);
   } 
 
-  
-/*
-  var form = new formidable.IncomingForm();
-  form.encoding = 'utf-8';
-  form.keepExtensions = false;
-  form.maxFieldsSize =  1073741824;  // 1GB max
-  form.maxFieldsSize =  1;  // 1GB max
-  form.maxFields = 100;  //100 images max
-
-  //Progress
-  form.on('progress', function(bytesReceived, bytesExpected) {
-    var progress = Math.round(bytesReceived/bytesExpected*100);
-    console.log('Progress: ' + progress + '%');
-    res.write('<p>Progress: ' + progress + '%<p>'); 
-  });
-
-  form.on('error', function(err) {
-    console.log("ERROR");
-  });
-
-  //Upload file
-  form.parse(req, function(err, fields, files) {
-    var oldpath = files.image.path;
-    var newpath = __dirname + '/public/gear360pano/to_pano.' + files.image.name;
-
-    //Create DB entry
-    var fileObj = {
-      path: newpath,
-      name : files.image.name
-    };
-    
-    fs.rename(oldpath, newpath, function(err) {
-      if(err)
-        throw err;
-      else
-      {
-        var stitch = exec(__dirname + '/public/gear360pano/gear360pano.sh -n ' + 
-                          '-o ' + __dirname + '/public/upload ' + 
-                          __dirname + '/public/gear360pano/to_pano*',
-          (error, stdout, stderr) => {
-            console.log(`${stdout}`);
-            console.log(`${stderr}`);
-            if(error !== null)
-              console.log(`exec error: ${error}`);
-          }
-        );
-
-        console.log('New image uploaded as:\n' + newpath);
-        res.write('<h1>Uploaded</h1>'); 
-        res.write("<a href='/'>Back</a>"); 
-      }
-      res.end();
-    });
-  });
-*/
+  res.write('<h1>Uploaded </h1>'); 
+  res.write("<a href='/'>Back</a>"); 
 }); 
 
 app.use(function (req, res, next) {
