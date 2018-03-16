6520b30b2e78dd49617e956998aee3abc88c7a06
Alex <aeiffert@ufl.edu>
2018-03-15 22:56:14 -0400

    using express-fileupload

--- devel/image_upload/server/index.js
+++ devel/image_upload/server/index.js
@@ -1,34 +1,62 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-
+var cloudinary = require('cloudinary');
 var exec = require('child_process').exec;
-var formidable = require('formidable');
-var fs = require('fs');
+var express = require('express');
+var fileUpload = require('express-fileupload');
 var path = require('path');
 
+var app = express();
+app.use(fileUpload());
+
+cloudinary.config({
+  cloud_name: 'serverful',
+  api_key: '627713686888251',
+  api_secret: '_GgV9Ru9rsEHFApnwTv_LedR-wg'
+});
+var upload_stream
+
+function uploadToServer(image, res) {
+  let date = Date.now();
+  console.log("Uploading " + image.name + " as: to_pano_" + date + ".png");
+
+  image.mv(__dirname + "/public/upload/to_pano_" + date + ".png", function(err) {
+    if(err)
+      return res.status(500).send(err);
+    res.write("Image(s) uploaded");
+  });
+} 
+
+function processImages() {
+}
+
+/**
+ * Root page
+ */
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+/**
+ * Post to upload page
+ */
 app.post('/upload', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  if(!req.files)
+    return res.status(400).send("No files were sent.");
 
+  let formData = req.files.image;
+  if(Array.isArray(formData)) {
+    console.log("Files incoming: " + req.files.image.length);
+    for(var i = 0; i < req.files.image.length; ++i) {
+      let image = formData[i];
+      uploadToServer(image, res);
+    }
+  }
+  else if(formData) {
+    console.log("Files incoming: 1");
+    uploadToServer(formData, res);
+  } 
+
+  
 /*
-  var files = [], fields = [];
-  form.on('field', function(field, value) {
-    fields.push([field, value]);
-  });
-  form.on('file', function(field, file) {
-    console.log(file.name);
-    files.push([field, file]);
-  });
-  form.on('end', function() {
-    console.log('done');
-    res.redirect('/');
-  });
-*/
-
   var form = new formidable.IncomingForm();
   form.encoding = 'utf-8';
   form.keepExtensions = false;
@@ -81,6 +109,7 @@ app.post('/upload', function(req, res) {
       res.end();
     });
   });
+*/
 }); 
 
 app.use(function (req, res, next) {
