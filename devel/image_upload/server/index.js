e41e8792d53f014d6c80c84e3e4f9ca9f33d2868
Alex <aeiffert@ufl.edu>
2018-03-15 21:26:04 -0400

    small changes

--- devel/image_upload/server/index.js
+++ devel/image_upload/server/index.js
@@ -2,18 +2,10 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-//var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 var formidable = require('formidable');
 var fs = require('fs');
 var path = require('path');
-
-//app.use(bodyParser.urlencoded({
-//  limit: '5mb',
-//  extended: true
-//}));
-//app.use(bodyParser.json());
-//app.get(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
@@ -59,9 +51,12 @@ app.post('/upload', function(req, res) {
   form.parse(req, function(err, fields, files) {
     var oldpath = files.image.path;
     var newpath = __dirname + '/public/gear360pano/to_pano.' + files.image.name;
-    //var fileObj = {};
-    //fileObj.name = files.image.name;
-    //fileObj.path = '/public/upload/';
+
+    //Create DB entry
+    var fileObj = {
+      path: newpath,
+      name : files.image.name
+    };
     
     fs.rename(oldpath, newpath, function(err) {
       if(err)
@@ -69,14 +64,14 @@ app.post('/upload', function(req, res) {
       else
       {
         var stitch = exec(__dirname + '/public/gear360pano/gear360pano.sh -n ' + 
-                            '-o ' + __dirname + '/public/upload ' + 
-                            __dirname + '/public/gear360pano/to_pano*',
-            (error, stdout, stderr) => {
-                console.log(`${stdout}`);
-                console.log(`${stderr}`);
-                if(error !== null)
-                  console.log(`exec error: ${error}`);
-            }
+                          '-o ' + __dirname + '/public/upload ' + 
+                          __dirname + '/public/gear360pano/to_pano*',
+          (error, stdout, stderr) => {
+            console.log(`${stdout}`);
+            console.log(`${stderr}`);
+            if(error !== null)
+              console.log(`exec error: ${error}`);
+          }
         );
 
         console.log('New image uploaded as:\n' + newpath);
