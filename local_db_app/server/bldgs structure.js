79fd29879fa1e5946d81bf138111ec4a9ade6aee
Marcial1234 <marcial.abrahantes@gmail.com>
2018-03-29 22:15:13 -0400

    Added state mapping files, and a help DB arch one

---
+++ local_db_app/server/bldgs structure.js
@@ -0,0 +1,11 @@
+"buildings collection/document": {
+  {
+    "id": "ref hash"
+    "City": "Miami",
+    "Name": "Warehouse",
+    "State": "FL",
+    "Long_State": "Florida",
+    "floors": 9,
+    "rooms": "ref",
+  }
+}
